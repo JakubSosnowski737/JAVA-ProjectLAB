1b6ebd5c8bbc237b453b40d097f49245a52c672d
Jakub <JakubSosnowski737@gmail.com>
2025-04-15 22:54:14 +0200

    Initialize project with Spring Boot and Maven Structure

---
+++ src/test/java/bookstore/projektjavalab/ProjektJavaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package bookstore.projektjavalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektJavaLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
